b4e464a584a485f601eae1d09206bcff74856a95
Fabio Cruz <fabio.rocha.cruz@tecnico.ulisboa.pt>
2020-03-22 21:42:55 +0000

    Memory factor implemented

--- Exercicio/src/Agent.java
+++ Exercicio/src/Agent.java
@@ -15,106 +15,189 @@ public class Agent {
 	String decision;
 	int restart;
 	double memoryFactor;
-	
-	int preparingStage;
+
+	int stepCounter;
+	int restartCounter;
 	String bestTask;
+	String currentTask;
 	double gain;
-	HashMap<String, Double> speculativeUtilities = new HashMap<String, Double>();
-	HashMap<String, Double> observedUtilities = new HashMap<String, Double>();
-	HashMap<String, Integer> utilityCounter = new HashMap<String, Integer>();
 	
+	HashMap<String, ArrayList<Double>> observedUtilities = new HashMap<String, ArrayList<Double>>();
+	HashMap<String, ArrayList<Integer>> utilitiesIndexes = new HashMap<String, ArrayList<Integer>>();
+	HashMap<String, Double> utilitiesAverage = new HashMap<String, Double>();
+
 	public Agent(String options) {
-		if (options.contains("cycle")) steps = Integer.parseInt(options.split("cycle=")[1].split(" ")[0].toString().trim());
-		if (options.contains("decision")) decision = options.split("decision=")[1].split(" ")[0].trim();
-		if (options.contains("restart")) restart = Integer.parseInt(options.split("restart=")[1].split(" ")[0].trim());
-		if (options.contains("memory-factor")) memoryFactor = Double.parseDouble(options.split("memory-factor=")[1].split(" ")[0].trim());
+		if (options.contains("cycle"))
+			steps = Integer.parseInt(options.split("cycle=")[1].split(" ")[0].toString().trim());
+		if (options.contains("decision"))
+			decision = options.split("decision=")[1].split(" ")[0].trim();
+		if (options.contains("restart"))
+			restart = Integer.parseInt(options.split("restart=")[1].split(" ")[0].trim());
+		if (options.contains("memory-factor"))
+			memoryFactor = Double.parseDouble(options.split("memory-factor=")[1].split(" ")[0].trim());
 	}
-	
+
 	public void perceive(String input) {
 		if (input.startsWith("T")) {
 			String task = input.split(" ")[0].toString();
 			double utility = Integer.parseInt(input.split("=")[1].toString().trim());
-			speculativeUtilities.put(task, utility);
+			utilitiesAverage.put(task, utility);
 		} else {
 			if (input.startsWith("A")) {
 				double newValue = Integer.parseInt(input.split("=")[1].toString().trim());
 				gain += newValue;
-				if (!observedUtilities.containsKey(bestTask)) {
-					speculativeUtilities.put(bestTask, newValue);
-					observedUtilities.put(bestTask, newValue);
-					utilityCounter.put(bestTask, 1);
+				if (!utilitiesIndexes.containsKey(bestTask)) {
+					ArrayList<Double> values = new ArrayList<Double>();
+					values.add(newValue);
+					observedUtilities.put(bestTask, values);
+					ArrayList<Integer> indexes = new ArrayList<Integer>();
+					indexes.add(stepCounter);
+					utilitiesIndexes.put(bestTask, indexes);
+					utilitiesAverage.put(bestTask, newValue);
 				} else {
-					double oldAverage = observedUtilities.get(bestTask);
-					int oldUtilityCounter = utilityCounter.get(bestTask);
-					double newAverage = oldAverage + ((newValue - oldAverage) / (oldUtilityCounter + 1));
-					speculativeUtilities.put(bestTask, newAverage);
-					observedUtilities.put(bestTask, newAverage);
-					utilityCounter.put(bestTask, oldUtilityCounter + 1);
+					ArrayList<Double> values = observedUtilities.get(bestTask);
+					values.add(newValue);
+					observedUtilities.put(bestTask, values);
+					ArrayList<Integer> indexes = utilitiesIndexes.get(bestTask);
+					indexes.add(stepCounter);
+					utilitiesIndexes.put(bestTask, indexes);
+					Double newAverage = memoryFactorAverage();
+					utilitiesAverage.put(bestTask, newAverage);
 				}
 			}
 		}
 	}
 	
+	private Double memoryFactorAverage() {
+		Double sum = 0.0;
+		for (Integer index: utilitiesIndexes.get(bestTask)) {
+			sum += Math.pow(index, memoryFactor);
+		}
+		Double average = 0.0;
+		int i=0;
+		for (Double value: observedUtilities.get(bestTask)) {
+			int valueIndex = utilitiesIndexes.get(bestTask).get(i);
+			Double valueProb = Math.pow(valueIndex, memoryFactor) / sum;
+			average += (valueProb*value);
+			i++;
+		}
+		return average;
+	}
+
 	public void decideAndAct() {
 		if (restart > 0) {
-			if (preparingStage == restart) {
-				bestTask = bestUtilityTask(speculativeUtilities);
-				preparingStage = 0;
+			stepCounter += 1;
+			HashMap<String, Double> decisionMap = new HashMap<String, Double>(utilitiesAverage);
+			if (bestTask != null) {
+				for (String task : decisionMap.keySet()) {
+					if (!task.equals(currentTask)) {
+						decisionMap.put(task, utilitiesAverage.get(task) - (double) restart);
+					}
+				}
+			}
+			System.out.println(decisionMap.toString());
+			System.out.println(stepCounter + " : " + restartCounter);
+			String task;
+			if (bestTask != null) {
+				task = bestRestartTask(decisionMap);
 			} else {
-				preparingStage += 1;
-			}	
+				task = bestUtilityTask(decisionMap);
+			}
+			if (stepCounter == steps) return;
+			if (restartCounter == restart) {
+				if (task.equals(currentTask)) bestTask = task;
+				restartCounter = 0;
+			} else {
+				if (!task.equals(currentTask)) {
+					restartCounter = 0;
+				} else {
+					if (bestTask != null && bestTask.equals(currentTask) && restartCounter == 1) {
+						restartCounter = 0;
+					}
+				}
+			}
+			restartCounter += 1;
+			currentTask = task;
+			System.out.println("current task " + currentTask);
+			System.out.println("best task " + bestTask);
+			System.out.println();
 		} else {
-			bestTask = bestUtilityTask(speculativeUtilities);
-		}	
+			stepCounter += 1;
+			bestTask = bestUtilityTask(utilitiesAverage);
+		}
 	}
-	
+
 	private String bestUtilityTask(HashMap<String, Double> map) {
-		String bestTask = null;
-		for (String task: map.keySet()) {
-			if (bestTask == null || map.get(task) > map.get(bestTask)) {
-				bestTask = task;
+		String currentBestTask = null;
+		for (String task : map.keySet()) {
+			if (currentBestTask == null || map.get(task) > map.get(currentBestTask)) {
+				currentBestTask = task;
 			} else {
-				if (map.get(task).compareTo(map.get(bestTask)) == 0) {
+				if (map.get(task).compareTo(map.get(currentBestTask)) == 0) {
 					int index1 = Integer.parseInt(task.split("T")[1].toString().trim());
-					int index2 = Integer.parseInt(bestTask.split("T")[1].toString().trim());
-					if (index1 < index2) bestTask = task;
+					int index2 = Integer.parseInt(currentBestTask.split("T")[1].toString().trim());
+					if (index1 < index2)
+						currentBestTask = task;
 				}
 			}
 		}
-		return bestTask;
+		return currentBestTask;
 	}
-	
+
+	private String bestRestartTask(HashMap<String, Double> map) {
+		String currentBestTask = null;
+		for (String task : map.keySet()) {
+			if (stepCounter == 5) {
+				System.out.println("comp task: " + task + " u: " + map.get(task));
+				System.out.println("comp current: " + currentBestTask + " u: " + map.get(currentBestTask));
+			}
+			if (currentBestTask == null || map.get(task) > map.get(currentBestTask)) {
+				currentBestTask = task;
+			} else {
+				if (map.get(task).compareTo(map.get(currentBestTask)) == 0 && !bestTask.equals(task) && !bestTask.equals(currentBestTask)) {
+					int index1 = Integer.parseInt(task.split("T")[1].toString().trim());
+					int index2 = Integer.parseInt(currentBestTask.split("T")[1].toString().trim());
+					if (index1 < index2)
+						currentBestTask = task;
+				}
+			}
+		}
+		return currentBestTask;
+	}
+
 	public void recharge() {
-		List<String> taskList = new ArrayList<>(speculativeUtilities.keySet());
+		List<String> taskList = new ArrayList<>(utilitiesAverage.keySet());
 		Collections.sort(taskList, (o1, o2) -> o1.compareTo(o2));
 
-		Locale locale  = new Locale("en", "UK");
-		DecimalFormat f = (DecimalFormat)NumberFormat.getNumberInstance(locale);
+		Locale locale = new Locale("en", "UK");
+		DecimalFormat f = (DecimalFormat) NumberFormat.getNumberInstance(locale);
 		f.applyPattern("#0.00");
 
 		System.out.print("state={");
 		int count = 0;
-		for (String task: taskList) {
+		for (String task : taskList) {
 			count += 1;
 			System.out.print(task + "=");
-			if (observedUtilities.get(task) != null) {
-				System.out.print(f.format(observedUtilities.get(task)));
+			if (utilitiesIndexes.get(task) != null) {
+				System.out.print(f.format(utilitiesAverage.get(task)));
 			} else {
 				System.out.print("NA");
 			}
-			if (count != speculativeUtilities.keySet().size()) System.out.print(",");
+			if (count != utilitiesAverage.keySet().size())
+				System.out.print(",");
 		}
 		System.out.print("} gain=" + f.format(gain));
 	}
 
-	
-    public static void main(String[] args) throws IOException { 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line = br.readLine();
 		Agent agent = new Agent(line);
-		while(!(line=br.readLine()).startsWith("end")) {
-			if(line.startsWith("TIK")) agent.decideAndAct();
-			else agent.perceive(line);
+		while (!(line = br.readLine()).startsWith("end")) {
+			if (line.startsWith("TIK"))
+				agent.decideAndAct();
+			else
+				agent.perceive(line);
 		}
 		agent.recharge();
 		br.close();
