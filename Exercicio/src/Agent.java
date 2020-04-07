a7062c10f33838bae44bcff8396962f34585e371
Fabio Cruz <fabio.rocha.cruz@tecnico.ulisboa.pt>
2020-04-07 20:20:43 +0100

    Flexible agents working

--- Exercicio/src/Agent.java
+++ Exercicio/src/Agent.java
@@ -27,6 +27,7 @@ public class Agent {
 	private HashMap<String, ArrayList<Integer>> utilitiesIndexes = new HashMap<String, ArrayList<Integer>>();
 	private HashMap<String, Double> utilitiesAverage = new HashMap<String, Double>();
 	
+	private HashMap<String, Double> flexibleProbabilities = new HashMap<String, Double>();
 	private ArrayList<String> negativeTasks = new ArrayList<String>();
 	
 	private Locale locale = new Locale("en", "UK");
@@ -47,44 +48,63 @@ public class Agent {
 			utilitiesAverage.put(task, utility);
 		} else {
 			if (input.startsWith("A")) {
-				double newValue = Integer.parseInt(input.split("=")[1].toString().trim());
-				if (newValue < 0 && decision.equals("flexible")) {
-					flexibleDecision = true;
-					negativeTasks.add(bestTask);
+				if (!flexibleDecision) {
+					double newValue = Integer.parseInt(input.split("=")[1].toString().trim());
+					if (newValue < 0 && decision.equals("flexible")) {
+						flexibleDecision = true;
+						negativeTasks.add(bestTask);
+					}
+					gain += newValue;
+					updateMaps(newValue, bestTask);
+					utilitiesAverage.put(bestTask, memoryFactorAverage(bestTask));
+				} else {
+					String task1 = input.split("=")[1].split("\\{")[1].toString().trim();
+					String task2 = input.split("=")[2].split(",")[1].toString().trim();
+					double utility1 = Double.parseDouble(input.split("=")[2].split(",")[0].toString().trim());
+					updateMaps(utility1, task1);
+					double utility2 = Double.parseDouble(input.split("=")[3].split("\\}")[0].toString().trim());
+					updateMaps(utility2, task2);
+					for (String task: flexibleProbabilities.keySet()) {
+						if (task.equals(task1)) {
+							gain += (utility1 * flexibleProbabilities.get(task));
+						} else {
+							gain += (utility2 * flexibleProbabilities.get(task));
+						}
+					}
+					utilitiesAverage.put(task1, memoryFactorAverage(task1));
+					utilitiesAverage.put(task2, memoryFactorAverage(task2));
+					flexibleDecision = false;
+					flexibleProbabilities.clear();
 				}
-				gain += newValue;
-				updateMaps(newValue);
-				if (!utilitiesIndexes.containsKey(bestTask)) utilitiesAverage.put(bestTask, newValue);
-				else utilitiesAverage.put(bestTask, memoryFactorAverage());
 			}
 		}
 	}
 	
-	private void updateMaps(Double newValue) {
+	private void updateMaps(Double newValue, String task) {
 		ArrayList<Double> values;
 		ArrayList<Integer> indexes;
-		if (utilitiesIndexes.containsKey(bestTask)) {
-			values = observedUtilities.get(bestTask);
-			indexes = utilitiesIndexes.get(bestTask);
+		if (utilitiesIndexes.containsKey(task)) {
+			values = observedUtilities.get(task);
+			indexes = utilitiesIndexes.get(task);
 		} else {
 			values = new ArrayList<Double>();
 			indexes = new ArrayList<Integer>();
 		}
 		values.add(newValue);
-		observedUtilities.put(bestTask, values);
+		observedUtilities.put(task, values);
 		indexes.add(stepCounter);
-		utilitiesIndexes.put(bestTask, indexes);
+		utilitiesIndexes.put(task, indexes);
 	}
 	
-	private Double memoryFactorAverage() {
+	private Double memoryFactorAverage(String task) {
 		Double sum = 0.0;
-		for (Integer index: utilitiesIndexes.get(bestTask)) {
+		for (Integer index: utilitiesIndexes.get(task)) {
 			sum += Math.pow(index, memoryFactor);
 		}
 		Double average = 0.0;
 		int i=0;
-		for (Double value: observedUtilities.get(bestTask)) {
-			int valueIndex = utilitiesIndexes.get(bestTask).get(i);
+		for (Double value: observedUtilities.get(task)) {
+			int valueIndex = utilitiesIndexes.get(task).get(i);
 			Double valueProb = Math.pow(valueIndex, memoryFactor) / sum;
 			average += (valueProb*value);
 			i++;
@@ -94,6 +114,7 @@ public class Agent {
 
 	public void decideAndAct() {
 		stepCounter++;
+		System.out.println("Step: " + stepCounter);
 		if (restart > 0) {
 			HashMap<String, Double> decisionMap = createRestartDecisionMap();
 			String task = bestUtilityTask(decisionMap);
@@ -113,30 +134,69 @@ public class Agent {
 			restartCounter += 1;
 			currentTask = task;
 		} else {
-			if (flexibleDecision) {
-				String negativeTask = bestTask;
-				String bestPositiveTask = bestFlexibleTask(utilitiesAverage, negativeTask);
-				System.out.println("Negative task: " + negativeTask);
-				System.out.println("Best positive: " + bestPositiveTask);
-				
-				
-				
+			if (decision.equals("flexible")) {
+				if (flexibleDecision) {
+					System.out.println(utilitiesAverage);
+					String negativeTask = bestTask;
+					String bestPositiveTask = bestFlexibleTask(utilitiesAverage);
+					bestTask = bestPositiveTask;
+					System.out.println("Negative task: " + negativeTask);
+					System.out.println("Best positive: " + bestPositiveTask);
+					Double absNegative = Math.abs(observedUtilities.get(negativeTask).get(observedUtilities.get(negativeTask).size() - 1));
+					Double bestPositiveUtility = utilitiesAverage.get(bestPositiveTask);
+					System.out.println("Negative abs: " + absNegative);
+					System.out.println("Positive utility: " + bestPositiveUtility);
+					Double prob1 = absNegative / (absNegative + bestPositiveUtility);
+					Double prob2 = bestPositiveUtility / (absNegative + bestPositiveUtility);
+					int index1 = Integer.parseInt(negativeTask.split("T")[1].toString().trim());
+					int index2 = Integer.parseInt(bestPositiveTask.split("T")[1].toString().trim());
+					String smallIndex, bigIndex;
+					if (index1 < index2) {
+						smallIndex = negativeTask;
+						bigIndex = bestPositiveTask;
+					} else {
+						smallIndex = bestPositiveTask;
+						bigIndex = negativeTask;
+					}
+					flexibleProbabilities.put(smallIndex, Math.max(prob1, prob2));
+					flexibleProbabilities.put(bigIndex, Math.min(prob1, prob2));
+					printFlexibleProbabilities();
+					System.out.println();
+				} else {
+					bestTask = bestFlexibleTask(utilitiesAverage);
+				}
+			} else {
+				bestTask = bestUtilityTask(utilitiesAverage);
 			}
-			bestTask = bestUtilityTask(utilitiesAverage);
 		}
 	}
 	
-	public String bestFlexibleTask(HashMap<String, Double> map, String negativeTask) {
+	private void printFlexibleProbabilities() {
+		System.out.print("{");
+		int count = 0;
+		for (String task: flexibleProbabilities.keySet()) {
+			count++;
+			System.out.print(task + "=" + frmt.format(flexibleProbabilities.get(task)));
+			if (count==1) {
+				System.out.print(",");
+			}
+		}
+		System.out.print("}");
+	}
+	
+	private String bestFlexibleTask(HashMap<String, Double> map) {
 		String currentBestTask = null;
 		for (String task : map.keySet()) {
-			if (currentBestTask == null || ((map.get(task) > map.get(currentBestTask)) && (map.get(task) >= 0) && (!task.equals(negativeTask)))) {
-				currentBestTask = task;
-			} else {
-				if (map.get(task).compareTo(map.get(currentBestTask)) == 0) {
-					int index1 = Integer.parseInt(task.split("T")[1].toString().trim());
-					int index2 = Integer.parseInt(currentBestTask.split("T")[1].toString().trim());
-					if (index1 < index2)
-						currentBestTask = task;
+			if ((map.get(task) >= 0) && (!negativeTasks.contains(task))) {
+				if (currentBestTask == null || map.get(task) > map.get(currentBestTask)) {
+					currentBestTask = task;
+				} else {
+					if (map.get(task).compareTo(map.get(currentBestTask)) == 0) {
+						int index1 = Integer.parseInt(task.split("T")[1].toString().trim());
+						int index2 = Integer.parseInt(currentBestTask.split("T")[1].toString().trim());
+						if (index1 < index2)
+							currentBestTask = task;
+					}
 				}
 			}
 		}
@@ -181,7 +241,7 @@ public class Agent {
 		System.out.print("state={");
 		int count = 0;
 		for (String task : taskList) {
-			count += 1;
+			count++;
 			System.out.print(task + "=");
 			if (utilitiesIndexes.get(task) != null) {
 				System.out.print(frmt.format(utilitiesAverage.get(task)));
